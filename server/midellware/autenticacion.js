182aee8d0f42f7984b88b3478647df0cd214a0fe
Rafael <rafaelrio4@gmail.com>
2020-11-13 20:38:52 -0300

    Commig google sign-in

--- server/midellware/autenticacion.js
+++ server/midellware/autenticacion.js
@@ -24,7 +24,7 @@ let verificaRolAdmin = (req, res, next) => {
 let verificaTk = (req, res, next) => {
 
     let token = req.get('Authorization');
-    console.log(token);
+    console.log(pñrocess.env.SEED);
     jwt.verify(token, process.env.SEED, (err, decoded) => {
 
         if (err) {
